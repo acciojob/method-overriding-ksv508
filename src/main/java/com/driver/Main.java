2b0d4114cf08578d458c4bc294f0876cb57a4b92
keshav <ksv508@gmail.com>
2023-06-10 11:50:34 +0530

    final

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,13 +8,14 @@ public class Main {
   }
   public static class B extends A{
     public String meth(){
-      return "Method is overridden in Extended class B";
+      return "Method is overridden in Extendend class B";
     }
   }
   public static void main(String[] args) {
+    // before overriding
     B b = new B();
     b.meth();
-
+  //after overriding
     B b1 = new B();
     b1.meth();
   }
